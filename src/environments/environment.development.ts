d4a71c72b8794e5651a58912e8f60c8ed5e2e6f8
= <=>
2024-08-14 12:27:14 +0530

    updated env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  gameServer: 'https://2p886xzx-5000.inc1.devtunnels.ms/',
+  gameServer: "https://casino-trustbet-dick.onrender.com/",
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  gameServer: 'https://2p886xzx-5000.inc1.devtunnels.ms/',
+  gameServer: 'https://casino-trustbet-dick.onrender.com/',
 };
